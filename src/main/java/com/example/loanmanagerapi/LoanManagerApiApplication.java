8e5ca599155bf8d45cbd1a74c400109c7eeda49e
Rayan1605 <76061189+Rayan1605@users.noreply.github.com>
2024-10-28 12:57:29 +0000

    Start

---
+++ src/main/java/com/example/loanmanagerapi/LoanManagerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.loanmanagerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanManagerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoanManagerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/loanmanagerapi/LoanManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.loanmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
